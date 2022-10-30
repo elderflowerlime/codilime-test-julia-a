f550a764d2aebc9050b22d75dcd08057304c983d
elderflowerlime <antczak.julia96@gmail.com>
2022-10-30 12:56:24 +0100

    part 1

---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <>
+    <div className="navbar">
+        <div className="navbar-container">
+            <ul className="navbar-list">
+                <li className="navbar-item"> 
+                <Link to='/' className="navbar-links"> Overview </Link>
+                </li>
+                <li className="navbar-item"> 
+                <Link to='/' className="navbar-links"> About </Link>
+                </li>
+                <li className="navbar-item"> 
+                <Link to='/' className="navbar-links"> Resources</Link>
+                </li>
+                <li className="navbar-item"> 
+                <Link to='/' className="navbar-links"> Network </Link>
+                </li>
+                <li className="navbar-item"> 
+                <Link to='/' className="navbar-links"> Cloud </Link>
+                </li>
+                <li className="navbar-item"> 
+                <Link to='/' className="navbar-links"> Download </Link>
+                </li>
+                <li className="navbar-item"> 
+                <Link to='/' className="navbar-links"> Contact </Link>
+                </li>
+            </ul>
+        </div>
+        <div className="navbar-contact-button">
+            <Link to='/' className="navbar-links"> 
+                <button className='btn'>Contact Us</button>
+            </Link>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
